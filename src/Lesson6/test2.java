f41d5dd4a4cb51d603d19dc7c47c637464c6fae1
a1234 <baldywrld@gmail.com>
2024-07-29 15:06:50 +0300

    test

--- src/Lesson6/test2.java
+++ src/Lesson6/test2.java
@@ -1,7 +1,14 @@
 package Lesson6;
 
+
+import java.util.Scanner;
+
 public class test2 {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        int b = 7;
+        for (int i=7; i<=98; i++){
+
+        }
+
     }
 }
